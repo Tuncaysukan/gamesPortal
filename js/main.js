5386b3cbf8494134a5e7174f83d53f4dc4a7496f
tuncay <tuncay@mekayazilim.com>
2021-03-11 10:24:36 +0300

    the end
    the end

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
+// CUSTOM OWL - CAROUSEL
 
-$(document).ready(function () {
-const owl=$(".owl-carousel").owlCarousel({
-        autoplay:false,
+$(document).ready(function(){
+        
+    let owl = $(".owl-carousel").owlCarousel({
+        autoplay :false,
         dots:true,
         dotsData:true,
         loop:true,
         nav:false,
-        items:1,
+        items: 1
+
+        });
+
+        $('.owl-dot').click(function() {
+            owl.trigger('to.owl.carousel',[$(this).index(), 1000]);
+        })
     });
 
-    $('.owl-dot').click(
-        function(){
-            owl.trigger('to.owl.carousel',[$(this).index(),1000])
-        }
-    )
+// OPEN -- CLOSE MENU
+
+$('#chek').change(function(){
+    if($(this).is(":checked")) {
+        $('.nav-header').addClass('active');
+    } else {
+        $('.nav-header').removeClass('active');
+    }
 });
